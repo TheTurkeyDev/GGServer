5756b433ccc834e4dd13ae641371c0475467867f
Turkey <turkey2349@gmail.com>
2018-06-27 09:35:55 -0400

    Finished displaying news info

--- src/main/java/com/theprogrammingturkey/ggserver/ui/NewsTab.java
+++ src/main/java/com/theprogrammingturkey/ggserver/ui/NewsTab.java
@@ -1,21 +1,30 @@
 package com.theprogrammingturkey.ggserver.ui;
 
-import com.theprogrammingturkey.ggserver.news.INewsData;
 import com.theprogrammingturkey.ggserver.news.NewsHolder;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.util.Callback;
 
 public class NewsTab extends Tab
 {
 	private ObservableList<NewsHolder> newsList;
+	private VBox details;
 
 	public NewsTab(Scene scene)
 	{
@@ -25,8 +34,9 @@ public class NewsTab extends Tab
 		screenSplit.setHgap(2);
 		screenSplit.setPadding(new Insets(10, 10, 10, 10));
 
-		newsList = FXCollections.observableArrayList(new NewsHolder(null, new TestData()), new NewsHolder(null, new TestData()));
+		newsList = FXCollections.observableArrayList();
 		ListView<NewsHolder> listView = new ListView<>(newsList);
+		listView.setPrefWidth(scene.getWidth() / 2);
 		listView.setCellFactory(new Callback<ListView<NewsHolder>, ListCell<NewsHolder>>()
 		{
 			@Override
@@ -38,40 +48,35 @@ public class NewsTab extends Tab
 
 		screenSplit.add(listView, 0, 0);
 
+		details = new VBox();
+		details.setPrefWidth(scene.getWidth() / 2);
+		details.setSpacing(5);
+		details.setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
+		listView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<NewsHolder>()
+		{
+			@Override
+			public void changed(ObservableValue<? extends NewsHolder> observable, NewsHolder oldValue, NewsHolder newValue)
+			{
+				if(newValue != null)
+				{
+					Text title = new Text(newValue.getNews().getTitle());
+					title.setFont(new Font(24));
+					details.getChildren().add(title);
+					details.setAlignment(Pos.TOP_CENTER);
+					Text body = new Text(newValue.getNews().getData());
+					body.setFont(new Font(15));
+					details.getChildren().add(body);
+				}
+			}
+		});
+
+		screenSplit.add(details, 1, 0);
+
 		this.setContent(screenSplit);
 	}
 
 	public void dispatchNews(NewsHolder news)
 	{
 		newsList.add(news);
-	}
-
-	static class TestData implements INewsData
-	{
-
-		@Override
-		public String getTitle()
-		{
-			return "test";
-		}
-
-		@Override
-		public String getDesc()
-		{
-			return "HELLO";
-		}
-
-		@Override
-		public String getData()
-		{
-			return "DAta";
-		}
-
-		@Override
-		public String getServiceID()
-		{
-			return "NULL";
-		}
-
 	}
 }
